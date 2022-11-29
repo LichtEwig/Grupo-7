ab1a46c37f9a6331732addd1e1e433911f0fbb45
Marduk57 <100881051+Marduk57@users.noreply.github.com>
2022-11-29 18:15:06 -0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
-let numero = parseInt (prompt('Bienvenida, por favor ingresa hora de cita: '))
+/*let numero = parseInt (prompt('Bienvenida, por favor ingresa hora de cita: '))
 
 let hour = 12
 let isWeekend = true;
 
 if (hour < 10 || hour > 18 || isWeekend) {
   alert("Disponible, por favor llenar el formulario de contato."); // Es fin de semana
-}
+}*/
